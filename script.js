ce14bf92c3986a64b9392b23377665f9cb9f85c0
Patricia Perez <patricia.perez0301@gmail.com>
2017-02-08 14:12:46 +0000

    I made a house with the turtle

---
+++ script.js
@@ -0,0 +1,17 @@
+TinyTurtle.apply(window);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(60);
+forward(23);
+right(60);
+forward(23);
+
+
+
